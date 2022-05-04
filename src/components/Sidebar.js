e045e5e824754fbe0dd51cd02a5269e6df384d9e
Nicolas <nicolassmoraes2005@gmail.com>
2022-05-04 16:52:54 -0300

    carousel corrigido

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import { Container, Content } from './StylesSidebar'
 import { 
   FaTimes, 
@@ -7,14 +8,14 @@ import {
   FaRegSun, 
   FaUserAlt, 
   FaIdCardAlt, 
-  FaRegFileAlt,
-  FaRegCalendarAlt,
-  FaChartBar
+  FaShoppingCart
 } from 'react-icons/fa'
 
 import SidebarItem from './SidebarItem'
 
 const Sidebar = ({ active }) => {
+
+  let navigate = useNavigate();
 
   const closeSidebar = () => {
     active(false)
@@ -24,13 +25,11 @@ const Sidebar = ({ active }) => {
     <Container sidebar={active}>
       <FaTimes onClick={closeSidebar} />  
       <Content>
-        <SidebarItem Icon={FaHome} Text="Home" />
-        <SidebarItem Icon={FaChartBar} Text="Statistics" />
-        <SidebarItem Icon={FaUserAlt} Text="Users" />
+        <SidebarItem Icon={FaHome} Text="Incio" onClick={ () => {navigate("/")}} />
+        <SidebarItem Icon={FaShoppingCart} Text="Carrinho" onClick={ () => {navigate("/carrinho")}} />
+        <SidebarItem Icon={FaUserAlt} Text="Perfil" />
         <SidebarItem Icon={FaEnvelope} Text="Mail" />
-        <SidebarItem Icon={FaRegCalendarAlt} Text="Calendar" />
         <SidebarItem Icon={FaIdCardAlt} Text="Employees" />
-        <SidebarItem Icon={FaRegFileAlt} Text="Reports" />
         <SidebarItem Icon={FaRegSun} Text="Settings" />
       </Content>
     </Container>
--- src/components/StylesSidebar.js
+++ src/components/StylesSidebar.js
@@ -9,6 +9,7 @@ export const Container = styled.div`
   width: 300px;
   left: ${props => props.sidebar ? '0' : '-100%'};
   animation: showSidebar .4s;
+  z-index:1000;
   > svg {
     text-align: left;
     position: fixed;
