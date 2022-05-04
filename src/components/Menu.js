e0b7506bb9a4f48769bccb706ec0acb6bd6b0592
Nicolas Sousa <nicolassmoraes2005@gmail.com>
2022-05-04 12:23:39 -0300

    Menu SideBar criado

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,9 +1,17 @@
 import "./Menu.css"
 import menu from "../assets/icons/menu.svg"
+import {useState} from 'react'
+import Sidebar from "./Sidebar"
 
 const Menu = () => {
+    const [sidebar, setSidebar] = useState(false)
+    const showSidebar = () => setSidebar(!sidebar)
+
     return (
-        <img src={menu} alt="Menu" className="menu" />
+        <>
+        <img src={menu} alt="Menu" className="menu" onClick={showSidebar} />
+        {sidebar && <Sidebar active = {setSidebar}/>}
+        </>
     )
 }
 
