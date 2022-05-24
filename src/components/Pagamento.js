221bbde7108745da1936bedf23aea3942554a05e
Nicolas <nicolassmoraes2005@gmail.com>
2022-05-24 16:07:37 -0300

    Tela de pagamento

--- src/components/Pagamento.js
+++ src/components/Pagamento.js
@@ -6,10 +6,10 @@ const Pagamento = () => {
     <div className="pagamento_form">
             <form className="form_pagamento">
                 <h1>Pagamento</h1>
-                <label for="nome">Nome:</label><input type="text" name="nome"/>
-                <label for="email">Email:</label><input type="email" name="email"/>
-                <label for="cpf">CPF:</label><input type="text" name="cpf"/>
-                <label for="telefone">Telefone:</label><input type="text" name="telefone"/>
+                <label for="nome" className='pag'>Nome:</label><input type="text" name="nome"/>
+                <label for="email" className='pag'>Email:</label><input type="email" name="email"/>
+                <label for="cpf" className='pag'>CPF:</label><input type="text" name="cpf"/>
+                <label for="telefone" className='pag'>Telefone:</label><input type="text" name="telefone"/>
                 <p>Selecione a forma de Pagamento:</p>
                 <div className='met_pag'>
                     <div className='PIX'>
@@ -25,11 +25,11 @@ const Pagamento = () => {
                         <label for="boleto">Boleto</label>
                     </div>
                 </div>
-                <label for="cep">CEP:</label><input type="text" name="cep"/>
-                <label for="cidade">Cidade:</label><input type="text" name="cidade"/>
-                <label for="estado">Estado:</label><input type="text" name="estado"/>
-                <label for="rua">Rua:</label><input type="text" name="rua"/>
-                <label for="numero">Numero:</label><input type="number" name="numero"/>
+                <label for="cep" className='pag'>CEP:</label><input type="text" name="cep"/>
+                <label for="cidade" className='pag'>Cidade:</label><input type="text" name="cidade"/>
+                <label for="estado" className='pag'>Estado:</label><input type="text" name="estado"/>
+                <label for="rua" className='pag'>Rua:</label><input type="text" name="rua"/>
+                <label for="numero" className='pag'>Numero:</label><input type="number" name="numero"/>
                 <input type="submit" value="Pagar Agora" className="botao_pagamento"/>
             </form>
         </div>
