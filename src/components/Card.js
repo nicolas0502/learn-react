462c0cef349b93d69ad0b0dcd21d4f26ce5da130
Nicolas Sousa <nicolassmoraes2005@gmail.com>
2022-06-10 01:37:18 -0300

    Carrinho desbugado

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,15 +2,33 @@ import "./Card.css"
 import Compra from "../assets/icons/compra.svg"
 import {Link} from "react-router-dom"
 import { useCarrinho } from "../providers/CarrinhoProvider"
-// import Produto from "./Produto"
+import { useAuth } from "../providers/AuthProvider"
 
 const Card = () => {
     const {idItens} = useCarrinho()
+    const {isLogged} = useAuth()
+
+    function userLogado() {
+        if(isLogged){
+            return(  
+                <>
+                <Link to="/carrinho"><img src={Compra} alt="Carrinho de compra" className="card"/></Link>
+                <div className="contador">{idItens.length}</div>
+                </>
+            )
+        }else{
+            return (
+                <>
+                <Link to="/login-usuario-ou-vendedor"><img src={Compra} alt="Carrinho de compra" className="card"/></Link>
+                <div className="contador">{idItens.length}</div>
+                </>
+            )
+        }
+    }
 
     return (
         <div className="compra">
-            <Link to="/carrinho"><img src={Compra} alt="Carrinho de compra" className="card"/></Link>
-            <div className="contador">{idItens.length}</div>
+            {userLogado()}
         </div>
     )
 }
--- src/components/ItensCarrinho.js
+++ src/components/ItensCarrinho.js
@@ -2,59 +2,79 @@ import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import AmountProd from './AmountProd';
 import "./ItensCarrinho.css"
+import { useCarrinho } from "../providers/CarrinhoProvider"
 
 const ItensCarrinho = () => {
     const [hqs, setHqs] = useState(null);
     const navigate = useNavigate()
-
+    const {idItens} = useCarrinho()
+    const quantItens = parseInt(idItens.length)
 
     useEffect(() => {
-        const carrinhodata = localStorage.getItem('carrinhoItens')
-		const carrinho = JSON.parse(carrinhodata)
-        const strigItens = carrinho.join(',')
-        fetch("http://localhost/LP2/api/hq/select-by-ids/?id="+strigItens)
+        if(quantItens !== 0){
+            const carrinhodata = localStorage.getItem('carrinhoItens')
+		    const carrinho = JSON.parse(carrinhodata)
+            const strigItens = carrinho.join(',')
+            fetch("http://localhost/LP2/api/hq/select-by-ids/?id="+strigItens)
             .then((response) => response.json())
             .then((data) => {setHqs(data)});
-    }, []);
+        }
+    }, [quantItens]);
+
+    function CardItens(){
+        if(quantItens !== 0){
+            return(
+                <>
+                <div className='iteem'>
+                    {hqs &&
+                        hqs.map((hq) => {
+                            return(
+                                <div className='item-carrinho' key={hq.id}>
+                                    <div className='item-imagem'>{hq.imagem}</div>
+                                    <div className='item-n-p'>
+                                        <div className="item-nome">{hq.nome}</div>
+                                        <div className='item-parcela'>Em 10x de R${((hq.valor / 10).toString().replace(".", ","))} sem juros</div>
+                                    </div>
+                                    <div className='item-add-quantidade'>
+                                        <div className="number-input">
+                                            <AmountProd value={hq.valor} quanti={hq.quantidade} />
+                                        </div>
+                                        <div className='item-quantidade'>
+                                            {hq.quantidade} Unidades
+                                        </div>
+                                    </div>
+                                </div>          
+                            )
+                        })
+                    }
+                </div>
+                <div>
+                    <div className='valor_total'>
+                        <p>
+                            Valor Total
+                        </p>
+                        <p>
+                            R$
+                        </p>
+                    </div>
+                    <div className='buy_now'>
+                        <button className="button_buy_now" onClick={() => {navigate('/pagamento')}}>Comprar Agora</button>
+                    </div>
+                </div>
+            </>
+            )
+        }else{
+            return(
+                <div className='carrinho_vazio'><h3>Não há intens no carrinho</h3></div>
+            )
+        }
+    }
+
+
 
   return (
     <div className='itenzinho'>
-        <div className='iteem'>
-        {hqs &&
-            hqs.map((hq) => {
-                return(
-                    <div className='item-carrinho' key={hq.id}>
-                        <div className='item-imagem'>{hq.imagem}</div>
-                        <div className='item-n-p'>
-                            <div className="item-nome">{hq.nome}</div>
-                            <div className='item-parcela'>Em 10x de R${((hq.valor / 10).toString().replace(".", ","))} sem juros</div>
-                        </div>
-                        <div className='item-add-quantidade'>
-                            <div className="number-input">
-                                <AmountProd value={hq.valor} quanti={hq.quantidade} />
-                            </div>
-                            <div className='item-quantidade'>
-                                {hq.quantidade} Unidades
-                            </div>
-                        </div>
-                    </div>          
-                )
-            })
-        }
-        </div>
-        <div>
-            <div className='valor_total'>
-                <p>
-                    Valor Total
-                </p>
-                <p>
-                    R$
-                </p>
-            </div>
-            <div className='buy_now'>
-                <button className="button_buy_now" onClick={() => {navigate('/pagamento')}}>Comprar Agora</button>
-            </div>
-        </div>
+        {CardItens()}
     </div>
   )
 }
