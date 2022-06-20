5f68d7b2e49a962c392cda9634ef2f744b8ff1a1
Nicolas Sousa <nicolassmoraes2005@gmail.com>
2022-06-20 10:23:01 -0300

    Atualização do modal

--- src/components/ModalAlerts.js
+++ src/components/ModalAlerts.js
@@ -3,14 +3,14 @@ import { Modal, Button } from "react-bootstrap"
 
 function ModalAlerts(props) {
     return (
-      <Modal {...props} aria-labelledby="contained-modal-title-vcenter">
+      <Modal {...props} aria-labelledby="contained-modal-title-vcenter" size="md">
         <Modal.Header closeButton>
           <Modal.Title id="contained-modal-title-vcenter">
             {props.title}
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-            <div className="mt-3 mb-3">
+            <div className="mt-2 mb-2">
                 {props.message}
             </div>
         </Modal.Body>
